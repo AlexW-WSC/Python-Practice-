446d18b13981d433a99d7444100d6f12de1d8718
AlexW-WSC <159727768+AlexW-WSC@users.noreply.github.com>
2025-05-08 12:13:38 +1200

    Milestone 1
    Code & notes for Milestone 1

---
+++ milestone_1/milestone_1_code.py
@@ -0,0 +1,4 @@
+name = input("What is your name?") # - gets name from input
+age = int(input("What is your age?")) # - gets age from input
+dog_age = age * 7 # - stores age in dog years into a new variable
+print("Hi {}! Your age is {} years. Your age in dog years is {} years.".format(name, age, dog_age)) # - prints the full statement 
