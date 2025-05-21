813626898f673ebc12882756ec05634e1b0dbf7b
AlexW-WSC <159727768+AlexW-WSC@users.noreply.github.com>
2025-05-22 11:17:00 +1200

    Finished Milestone 2

--- milestone_2/milestone_2_code.py
+++ milestone_2/milestone_2_code.py
@@ -1,9 +1,10 @@
+# Define constant, define each check string
 STRONG_PASSWORD_LENGTH = 10
 special_chars = "!@#$%^&*()-+_=<>?/"
 numbers = "1234567890"
 uppercase_letters = "QWERTYUIOPASDFGHJKLZXCVBNM"
 lowercase_letters = "qwertyuiopasdfghjklzxcvbnm"
-password = None
+# Introductory print statements
 print(" ")
 print("Test your password strength~!")
 print("-----" \
@@ -12,9 +13,10 @@ print("DISCLAIMER: no data from this program is stored anywhere. The code for th
 print("-----" \
 "")
 print(" ")
-
+# Ask user for password
 password = input("What is your proposed password?").strip()
 
+# Checks password for correct length, presence of both cases, presence of either a number or a special character
 if len(password) >= STRONG_PASSWORD_LENGTH and any(i in uppercase_letters for i in password) and any(i in lowercase_letters for i in password) and (any(i in special_chars for i in password) or any(i in numbers for i in password)):
     print("You've got a long password({} characters), with a mix of cases, and a special character or number in there too. Great job!!".format(len(password)))
 elif len(password) >= STRONG_PASSWORD_LENGTH and any(i in uppercase_letters for i in password) and any(i in lowercase_letters for i in password):
@@ -22,7 +24,8 @@ elif len(password) >= STRONG_PASSWORD_LENGTH and any(i in uppercase_letters for 
 elif len(password) >= STRONG_PASSWORD_LENGTH:
     print("Good start with the password length, but you should really try switching up the case, or adding special characters (symbols/numbers)")
 else: 
-    print("This password is too short. That should be your first concern; increasing the character length. Then make sure it's diverse as well, with different case and use of special characters.")
+    print("This password is too short. That should be your first concern; increasing the character length to at least to at least {} characters. Then make sure it's diverse as well, with different case and use of special characters.".format(STRONG_PASSWORD_LENGTH))
 
+# Removes the password variable at the end of the script for safety
 del(password)
 
